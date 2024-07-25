d428976d6499a3e32580999c7a30473de61e7e44
JulioChacon <jcchacon3000@gmail.com>
2024-07-25 02:10:24 -0700

    Added Tabs Screens and Tabs properties config.

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,8 +1,33 @@
 import { Tabs } from 'expo-router';
-import { View } from 'react-native';
 
 import { HomeIcon, InfoIcon } from '../../components/Icons';
 
 export default function TabsLayout() {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'yellow',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: '#000',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'Sobre nosotros',
+          tabBarIcon: ({ color }) => <InfoIcon color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
